fa0bf57413428cc62e11c26883013bc7c719f0e0
Marta Braga <ist110034@tecnico.ulisboa.pt>
2024-12-15 21:07:51 +0000

    mudanca de vetores para queue e set

--- file.cpp
+++ file.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
+#include <queue>
 
 using namespace std;
 
@@ -8,16 +10,11 @@ struct Node {
     int id;
     bool seen = false;
     int distance = -1;
-    vector<Node*> neighbors;
+    unordered_set<Node*> neighbors;
 
     Node(int id) : id(id) {}
-    void addNeighbor(Node* neighbor) { // pode valer a pena usar um set
-        for (Node* n : neighbors) {
-            if (n->id == neighbor->id) {
-                return;
-            }
-        }   
-        neighbors.push_back(neighbor);
+    void addNeighbor(Node* neighbor) {
+        neighbors.insert(neighbor);
     }
 };
 
@@ -44,16 +41,16 @@ void readInput(int stations, int lines, unordered_map<int, Node*>& graph) {
 }
 
 int calculateDistance(unordered_map<int, Node*>& graph, int numNodes) {
-    vector<Node*> queue;
+    queue<Node*> queue;
     Node* start = graph[1];
     start->distance = 0;
-    queue.push_back(start);
+    queue.push(start);
     int maxDistance = 0;
     int seen = 0;
     while (!queue.empty()) {
         Node* current = queue.front();
         current->seen = true;
-        queue.erase(queue.begin());
+        queue.pop();
         seen++;
         for (Node* neighbor : current->neighbors) {
             if (neighbor->seen) {
@@ -61,15 +58,25 @@ int calculateDistance(unordered_map<int, Node*>& graph, int numNodes) {
             }
             if (neighbor->distance == -1 || neighbor->distance > current->distance + 1) {
                 neighbor->distance = current->distance + 1;
-                queue.push_back(neighbor);
-                maxDistance = max(maxDistance, neighbor->distance);
+                queue.push(neighbor);
             }
         }
     }
     if(seen < numNodes) {
         return -1;
     }
-    return(maxDistance / 2 - 1);
+    for (auto& pair : graph) {
+        Node* node = pair.second;
+        if (node->distance > maxDistance) {
+            maxDistance = node->distance;
+        }
+    }
+        for (const auto& pair : graph) {
+        Node* node = pair.second;
+        cout << "Node " << node->id << " has distance " << node->distance << endl;
+    }
+    cout << "Max distance: " << maxDistance << endl;
+    return((maxDistance + 1) / 2 - 1);
 }
 
 int main() {
@@ -90,5 +97,7 @@ int main() {
     unordered_map<int, Node*> graph;
     readInput(stations, lines, graph);
     cout << calculateDistance(graph, numNodes) << "\n";
+
+
     return 0;
 }
